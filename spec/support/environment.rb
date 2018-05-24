41f6f54de2b262f7abec3690bf5a039653ad520f
Watson <wtsnjp@gmail.com>
2018-05-24 13:12:58 +0900

    independent from the "type" command

--- spec/support/environment.rb
+++ spec/support/environment.rb
@@ -1,6 +1,18 @@
 require 'aruba/rspec'
 require 'pathname'
 
+# closs-platform "which" from https://stackoverflow.com/questions/2108727/
+def which(cmd)
+  exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+  ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+    exts.each { |ext|
+      exe = File.join(path, "#{cmd}#{ext}")
+      return exe if File.executable?(exe) && !File.directory?(exe)
+    }
+  end
+  return nil
+end
+
 RSpec.shared_context "environment" do
   # check the existence of the texdoc source
   texdoc_script_dir = Pathname.pwd.join("script")
@@ -10,7 +22,7 @@ RSpec.shared_context "environment" do
 
   # check the existence of texlua
   texlua = "texlua"
-  fail("Command #{texlua} does not exist") if `type #{texlua}`.empty?
+  fail("Command #{texlua} does not exist") if !which(texlua)
 
   # use a controled environment
   RSpec.configure do |config|
@@ -20,7 +32,7 @@ RSpec.shared_context "environment" do
     end
   end
 
-  # constract texdoc command
+  # compose the target texdoc command
   texdoc = texdoc_script_dir.join("texdoc.tlu")
   fail("File #{texdoc} does not exists") if !texdoc.file?
   let(:cmd) { texlua + " " + texdoc.to_s }
