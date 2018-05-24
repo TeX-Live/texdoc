8a877adad9d210be0b5a73abc0e8dbb94873093c
Watson <wtsnjp@gmail.com>
2018-05-24 16:35:25 +0900

    create pseudo texmf dynamically

--- spec/support/environment.rb
+++ spec/support/environment.rb
@@ -1,5 +1,6 @@
 require 'aruba/rspec'
 require 'pathname'
+require 'fileutils'
 
 # closs-platform "which" from https://stackoverflow.com/questions/2108727/
 def which(cmd)
@@ -15,10 +16,16 @@ end
 
 RSpec.shared_context "environment" do
   # check the existence of the texdoc source
-  texdoc_script_dir = Pathname.pwd.join("script")
-  texdoc_spec_dir = Pathname.pwd.join("spec")
-  fail("Directory #{texdoc_script_dir} does not exists") if !texdoc_script_dir.directory?
-  fail("Directory #{texdoc_spec_dir} does not exists") if !texdoc_spec_dir.directory?
+  texdoc_scriptdir = Pathname.pwd.join("script")
+  texdoc_cnf = Pathname.pwd.join("texdoc.cnf")
+  texdoc_tlu = texdoc_scriptdir.join("texdoc.tlu")
+  fail("Directory #{texdoc_scriptdir} does not exists") if !texdoc_scriptdir.directory?
+  fail("File #{texdoc_cnf} does not exists") if !texdoc_cnf.file?
+  fail("File #{texdoc_tlu} does not exists") if !texdoc_tlu.file?
+
+  # prepare for pseudo TEXMF
+  texdoc_tmpdir = Pathname.pwd.join("tmp")
+  texmf = Pathname(texdoc_tmpdir).join("texmf")
 
   # check the existence of texlua
   texlua = "texlua"
@@ -27,13 +34,24 @@ RSpec.shared_context "environment" do
   # use a controled environment
   RSpec.configure do |config|
     config.before(:suite) do
-      ENV["TEXMFHOME"] = texdoc_spec_dir.join("texmf").to_s
+      # construct pseudo TEXMF
+      texmf_scripts = texmf.join("scripts")
+      texmf_texdoc = texmf.join("texdoc")
+      FileUtils.mkdir_p([texmf_scripts, texmf_texdoc])
+      FileUtils.ln_s(texdoc_scriptdir, texmf_scripts.join("texdoc"))
+      FileUtils.ln_s(texdoc_cnf, texmf_texdoc.join("texdoc.cnf"))
+
+      # set ENV
+      ENV["TEXMFHOME"] = texmf.to_s
       ENV["LC_ALL"] = "C"
+    end
+
+    config.after(:suite) do
+      # remove pseudo TEXMF
+      FileUtils.remove_dir(texmf, force: true)
     end
   end
 
   # compose the target texdoc command
-  texdoc = texdoc_script_dir.join("texdoc.tlu")
-  fail("File #{texdoc} does not exists") if !texdoc.file?
-  let(:cmd) { texlua + " " + texdoc.to_s }
+  let(:cmd) { texlua + " " + texdoc_tlu.to_s }
 end
