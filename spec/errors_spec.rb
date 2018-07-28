ee89b377a10a5ad9ed4c5daf36b2dd7f9dba2aad
Watson <wtsnjp@gmail.com>
2018-07-28 19:09:57 +0900

    no file existence check in the view_file() function (cf. #18)

--- spec/errors_spec.rb
+++ spec/errors_spec.rb
@@ -28,13 +28,6 @@ RSpec.describe "Error case:", :type => :aruba do
       error_line "Try `texdoc --help' for short help, `texdoc texdoc' for full manual.") }
   end
 
-  context "try to open not existing file with --just-view" do
-    before(:each) { run_texdoc "--just-view", "not_exist" }
-    before(:each) { stop_all_commands }
-    it { expect(last_command_started).to have_exit_status(1) }
-    it { expect(stderr).to include(error_line 'File "not_exist" does not exist.') }
-  end
-
   context "when any document for input cannot be found" do
     before(:each) { run_texdoc "never_never_existing_package_foooooooooo" }
     before(:each) { stop_all_commands }
