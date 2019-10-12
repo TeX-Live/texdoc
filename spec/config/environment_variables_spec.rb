5099b6ee037b44d439869a5c07fe7186ebfbed02
Eisuke Kawashima <e-kwsm@users.noreply.github.com>
2019-10-13 02:41:00 +0900

    Implement `SpecHelplers::Texdoc::MOCK_VIEWER` for convenience during test

--- spec/config/environment_variables_spec.rb
+++ spec/config/environment_variables_spec.rb
@@ -13,118 +13,120 @@ RSpec.describe "Environment variable", :type => :aruba do
       "Ignoring \"#{config}\" from environment variable \"#{env}\"."
   end
 
+  let(:mock_viewer) { SpecHelplers::Texdoc::MOCK_VIEWER }
+
   if not OS.windows?
     context "BROWSER" do
       before(:each) { delete_environment_variable "BROWSER_texdoc" }
-      before(:each) { set_environment_variable "BROWSER", ":" }
+      before(:each) { set_environment_variable "BROWSER", mock_viewer }
       before(:each) { run_texdoc "-dconfig", "-lI", "texlive-en" }
 
       it "should be effective" do
-        expect(stderr).to include(set_env_line "viewer_html=:", "BROWSER")
+        expect(stderr).to include(set_env_line "viewer_html=#{mock_viewer}", "BROWSER")
       end
     end
   end
 
   context "BROWSER_texdoc" do
-    before(:each) { set_environment_variable "BROWSER", ":" }
-    before(:each) { set_environment_variable "BROWSER_texdoc", ":" }
+    before(:each) { set_environment_variable "BROWSER", mock_viewer }
+    before(:each) { set_environment_variable "BROWSER_texdoc", mock_viewer }
     before(:each) { run_texdoc "-dconfig", "-lI", "texlive-en" }
 
     it "should be effective and given priority to BROWSER" do
-      expect(stderr).to include(set_env_line "viewer_html=:", "BROWSER_texdoc")
-      expect(stderr).to include(ignore_env_line "viewer_html=:", "BROWSER")
+      expect(stderr).to include(set_env_line "viewer_html=#{mock_viewer}", "BROWSER_texdoc")
+      expect(stderr).to include(ignore_env_line "viewer_html=#{mock_viewer}", "BROWSER")
     end
   end
 
   if not OS.windows?
     context "DVIVIEWER" do
       before(:each) { delete_environment_variable "DVIVIEWER_texdoc" }
-      before(:each) { set_environment_variable "DVIVIEWER", ":" }
+      before(:each) { set_environment_variable "DVIVIEWER", mock_viewer }
       before(:each) { run_texdoc "-dconfig", "-lI", "texlive-en" }
 
       it "should be effective" do
-        expect(stderr).to include(set_env_line "viewer_dvi=:", "DVIVIEWER")
+        expect(stderr).to include(set_env_line "viewer_dvi=#{mock_viewer}", "DVIVIEWER")
       end
     end
   end
 
   context "DVIVIEWER_texdoc" do
-    before(:each) { set_environment_variable "DVIVIEWER", ":" }
-    before(:each) { set_environment_variable "DVIVIEWER_texdoc", ":" }
+    before(:each) { set_environment_variable "DVIVIEWER", mock_viewer }
+    before(:each) { set_environment_variable "DVIVIEWER_texdoc", mock_viewer }
     before(:each) { run_texdoc "-dconfig", "-lI", "texlive-en" }
 
     it "should be effective and given priority to DVIVIEWER" do
-      expect(stderr).to include(set_env_line "viewer_dvi=:", "DVIVIEWER_texdoc")
-      expect(stderr).to include(ignore_env_line "viewer_dvi=:", "DVIVIEWER")
+      expect(stderr).to include(set_env_line "viewer_dvi=#{mock_viewer}", "DVIVIEWER_texdoc")
+      expect(stderr).to include(ignore_env_line "viewer_dvi=#{mock_viewer}", "DVIVIEWER")
     end
   end
 
   if not OS.windows?
     context "MDVIEWER" do
       before(:each) { delete_environment_variable "MDVIEWER_texdoc" }
-      before(:each) { set_environment_variable "MDVIEWER", ":" }
+      before(:each) { set_environment_variable "MDVIEWER", mock_viewer }
       before(:each) { run_texdoc "-dconfig", "-lI", "texlive-en" }
 
       it "should be effective" do
-        expect(stderr).to include(set_env_line "viewer_md=:", "MDVIEWER")
+        expect(stderr).to include(set_env_line "viewer_md=#{mock_viewer}", "MDVIEWER")
       end
     end
   end
 
   context "MDVIEWER_texdoc" do
-    before(:each) { set_environment_variable "MDVIEWER", ":" }
-    before(:each) { set_environment_variable "MDVIEWER_texdoc", ":" }
+    before(:each) { set_environment_variable "MDVIEWER", mock_viewer }
+    before(:each) { set_environment_variable "MDVIEWER_texdoc", mock_viewer }
     before(:each) { run_texdoc "-dconfig", "-lI", "texlive-en" }
 
     it "should be effective and given priority to MDVIEWER" do
-      expect(stderr).to include(set_env_line "viewer_md=:", "MDVIEWER_texdoc")
-      expect(stderr).to include(ignore_env_line "viewer_md=:", "MDVIEWER")
+      expect(stderr).to include(set_env_line "viewer_md=#{mock_viewer}", "MDVIEWER_texdoc")
+      expect(stderr).to include(ignore_env_line "viewer_md=#{mock_viewer}", "MDVIEWER")
     end
   end
 
   if not OS.windows?
     context "PAGER" do
       before(:each) { delete_environment_variable "PAGER_texdoc" }
-      before(:each) { set_environment_variable "PAGER", ":" }
+      before(:each) { set_environment_variable "PAGER", mock_viewer }
       before(:each) { run_texdoc "-dconfig", "-lI", "texlive-en" }
 
       it "should be effective" do
-        expect(stderr).to include(set_env_line "viewer_txt=:", "PAGER")
+        expect(stderr).to include(set_env_line "viewer_txt=#{mock_viewer}", "PAGER")
       end
     end
   end
 
   context "PAGER_texdoc" do
-    before(:each) { set_environment_variable "PAGER", ":" }
-    before(:each) { set_environment_variable "PAGER_texdoc", ":" }
+    before(:each) { set_environment_variable "PAGER", mock_viewer }
+    before(:each) { set_environment_variable "PAGER_texdoc", mock_viewer }
     before(:each) { run_texdoc "-dconfig", "-lI", "texlive-en" }
 
     it "should be effective and given priority to PAGER" do
-      expect(stderr).to include(set_env_line "viewer_txt=:", "PAGER_texdoc")
-      expect(stderr).to include(ignore_env_line "viewer_txt=:", "PAGER")
+      expect(stderr).to include(set_env_line "viewer_txt=#{mock_viewer}", "PAGER_texdoc")
+      expect(stderr).to include(ignore_env_line "viewer_txt=#{mock_viewer}", "PAGER")
     end
   end
 
   if not OS.windows?
     context "PDFVIEWER" do
       before(:each) { delete_environment_variable "PDFVIEWER_texdoc" }
-      before(:each) { set_environment_variable "PDFVIEWER", ":" }
+      before(:each) { set_environment_variable "PDFVIEWER", mock_viewer }
       before(:each) { run_texdoc "-dconfig", "-lI", "texlive-en" }
 
       it "should be effective" do
-        expect(stderr).to include(set_env_line "viewer_pdf=:", "PDFVIEWER")
+        expect(stderr).to include(set_env_line "viewer_pdf=#{mock_viewer}", "PDFVIEWER")
       end
     end
   end
 
   context "PDFVIEWER_texdoc" do
-    before(:each) { set_environment_variable "PDFVIEWER", ":" }
-    before(:each) { set_environment_variable "PDFVIEWER_texdoc", ":" }
+    before(:each) { set_environment_variable "PDFVIEWER", mock_viewer }
+    before(:each) { set_environment_variable "PDFVIEWER_texdoc", mock_viewer }
     before(:each) { run_texdoc "-dconfig", "-lI", "texlive-en" }
 
     it "should be effective and given priority to PDFVIEWER" do
-      expect(stderr).to include(set_env_line "viewer_pdf=:", "PDFVIEWER_texdoc")
-      expect(stderr).to include(ignore_env_line "viewer_pdf=:", "PDFVIEWER")
+      expect(stderr).to include(set_env_line "viewer_pdf=#{mock_viewer}", "PDFVIEWER_texdoc")
+      expect(stderr).to include(ignore_env_line "viewer_pdf=#{mock_viewer}", "PDFVIEWER")
     end
   end
 
@@ -132,23 +134,23 @@ RSpec.describe "Environment variable", :type => :aruba do
   if not OS.windows?
     context "PSVIEWER" do
       before(:each) { delete_environment_variable "PSVIEWER_texdoc" }
-      before(:each) { set_environment_variable "PSVIEWER", ":" }
+      before(:each) { set_environment_variable "PSVIEWER", mock_viewer }
       before(:each) { run_texdoc "-dconfig", "-lI", "texlive-en" }
 
       it "should be effective" do
-        expect(stderr).to include(set_env_line "viewer_ps=:", "PSVIEWER")
+        expect(stderr).to include(set_env_line "viewer_ps=#{mock_viewer}", "PSVIEWER")
       end
     end
   end
 
   context "PSVIEWER_texdoc" do
-    before(:each) { set_environment_variable "PSVIEWER", ":" }
-    before(:each) { set_environment_variable "PSVIEWER_texdoc", ":" }
+    before(:each) { set_environment_variable "PSVIEWER", mock_viewer }
+    before(:each) { set_environment_variable "PSVIEWER_texdoc", mock_viewer }
     before(:each) { run_texdoc "-dconfig", "-lI", "texlive-en" }
 
     it "should be effective and given priority to PSVIEWER" do
-      expect(stderr).to include(set_env_line "viewer_ps=:", "PSVIEWER_texdoc")
-      expect(stderr).to include(ignore_env_line "viewer_ps=:", "PSVIEWER")
+      expect(stderr).to include(set_env_line "viewer_ps=#{mock_viewer}", "PSVIEWER_texdoc")
+      expect(stderr).to include(ignore_env_line "viewer_ps=#{mock_viewer}", "PSVIEWER")
     end
   end
 
