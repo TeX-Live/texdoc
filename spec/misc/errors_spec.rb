0a7a7a6315d0e39008236ccfdfce46112b064af0
Takuto Asakura <wtsnjp@gmail.com>
2024-02-02 11:57:35 +0900

    proper error handling for unknown options (fix #110)

--- spec/misc/errors_spec.rb
+++ spec/misc/errors_spec.rb
@@ -3,34 +3,6 @@ require 'spec_helper'
 RSpec.describe "Errors", :type => :aruba do
   include_context "messages"
 
-  context "running without any option nor argument" do
-    before(:each) { run_texdoc }
-
-    it 'should result in the "no action" error' do
-      expect(last_command_started).to have_exit_status(2)
-      expect(stderr).to include(error_line "No action specified.")
-      expect(stderr).to include(error_line msg_usage)
-    end
-  end
-
-  context "missing arguments for Option -d" do
-    before(:each) { run_texdoc "-d" }
-
-    it 'should result in a getopt parser error' do
-      expect(last_command_started).to have_exit_status(1)
-      expect(stderr).to include(error_line "Option -d requires an argument.")
-    end
-  end
-
-  context "missing arguments for Option -c" do
-    before(:each) { run_texdoc "-c" }
-
-    it 'should result in a getopt parser error' do
-      expect(last_command_started).to have_exit_status(1)
-      expect(stderr).to include(error_line "Option -c requires an argument.")
-    end
-  end
-
   context "when any document for input cannot be found" do
     let(:nonexist_pkg) { "never_never_existing_package_foooooooooo" }
 
--- spec/misc/getopt_spec.rb
+++ spec/misc/getopt_spec.rb
@@ -110,4 +110,42 @@ RSpec.describe "The command-line option parser", :type => :aruba do
         debug_line "search", "Searching documents for pattern \"-texlive-en\"")
     end
   end
+
+  # error cases
+  context "running without any option nor argument" do
+    before(:each) { run_texdoc }
+
+    it 'should result in the "no action" error' do
+      expect(last_command_started).to have_exit_status(2)
+      expect(stderr).to include(error_line "No action specified.")
+      expect(stderr).to include(error_line msg_usage)
+    end
+  end
+
+  context "missing arguments for Option -d" do
+    before(:each) { run_texdoc "-d" }
+
+    it "should result in a getopt parser error" do
+      expect(last_command_started).to have_exit_status(1)
+      expect(stderr).to include(error_line "Option -d requires an argument.")
+    end
+  end
+
+  context "missing arguments for Option -c" do
+    before(:each) { run_texdoc "-c" }
+
+    it "should result in a getopt parser error" do
+      expect(last_command_started).to have_exit_status(1)
+      expect(stderr).to include(error_line "Option -c requires an argument.")
+    end
+  end
+
+  context "given an unknown option" do
+    before(:each) { run_texdoc "-x" }
+
+    it "should result in a getopt parser error" do
+      expect(last_command_started).to have_exit_status(2)
+      expect(stderr).to include(error_line "unknown option: -x")
+    end
+  end
 end
