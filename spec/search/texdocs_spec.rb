edc0e84d991ba2318388b1c416fd0b308c7043c2
Takuto Asakura <wtsnjp@gmail.com>
2024-08-10 22:19:00 +0900

    more w32_path for debug outputs

--- spec/search/texdocs_spec.rb
+++ spec/search/texdocs_spec.rb
@@ -11,19 +11,21 @@ RSpec.describe "TEXDOCS handling", :type => :aruba do
     }
     before(:each) { run_texdoc "-dtexdocs", "listings" }
 
+    let(:tree_prefix) { normalize_path("path/to/") }
+
     it "should be properly interpreted" do
       expect(stderr).to include(
         debug_line "texdocs",
-          "texdocs[4] = path/to/tree4 (index_mandatory=false, recursion_allowed=false)")
+          "texdocs[4] = #{tree_prefix}tree4 (index_mandatory=false, recursion_allowed=false)")
       expect(stderr).to include(
         debug_line "texdocs",
-          "texdocs[3] = path/to/tree3 (index_mandatory=false, recursion_allowed=true)")
+          "texdocs[3] = #{tree_prefix}tree3 (index_mandatory=false, recursion_allowed=true)")
       expect(stderr).to include(
         debug_line "texdocs",
-          "texdocs[2] = path/to/tree2 (index_mandatory=true, recursion_allowed=false)")
+          "texdocs[2] = #{tree_prefix}tree2 (index_mandatory=true, recursion_allowed=false)")
       expect(stderr).to include(
         debug_line "texdocs",
-          "texdocs[1] = path/to/tree1 (index_mandatory=true, recursion_allowed=true)")
+          "texdocs[1] = #{tree_prefix}tree1 (index_mandatory=true, recursion_allowed=true)")
     end
   end
 
@@ -33,15 +35,19 @@ RSpec.describe "TEXDOCS handling", :type => :aruba do
     let(:texmf_dist_regex) { Regexp.escape(normalize_path(`kpsewhich -var-value TEXMFDIST`.chomp)) }
     let(:texmf_home_regex) { Regexp.escape(normalize_path(texmf_home)) }
 
+    let(:path_doc_suffix_regex) { Regexp.escape(normalize_path("/doc")) }
+    let(:shift_doc_suffix_regex) { Regexp.escape(normalize_path("doc/")) }
+
     let(:texdocs_n_dist) {
-      /texdocs\[(\d+?)\] = #{texmf_dist_regex}\/doc/.match(stderr).to_a.values_at(1)[0].to_i
+      /texdocs\[(\d+?)\] = #{texmf_dist_regex}#{path_doc_suffix_regex}/.match(stderr).to_a.values_at(1)[0].to_i
     }
     let(:texdocs_n_home) {
-      /texdocs\[(\d+?)\] = #{texmf_home_regex}\/doc/.match(stderr).to_a.values_at(1)[0].to_i
+      /texdocs\[(\d+?)\] = #{texmf_home_regex}#{path_doc_suffix_regex}/.match(stderr).to_a.values_at(1)[0].to_i
     }
 
     it "should search TEXMFDIST using index" do
-      expect(stderr).to match(/texdocs\[#{texdocs_n_dist}\] using index: #{texmf_dist_regex} \(shift=doc\/\)/)
+      expect(stderr).to match(
+        /texdocs\[#{texdocs_n_dist}\] using index: #{texmf_dist_regex} \(shift=#{shift_doc_suffix_regex}\)/)
     end
 
     it "should search TEXMFHOME using file system" do
