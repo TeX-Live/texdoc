dba5b3d5f75db32583d6660389b640fc7a5425e9
Takuto Asakura <wtsnjp@gmail.com>
2026-07-18 14:54:28 -0600

    Check whether doclist is not empty before trying to open or list it (fix #131)

--- spec/misc/errors_spec.rb
+++ spec/misc/errors_spec.rb
@@ -2,14 +2,23 @@ require 'spec_helper'
 
 RSpec.describe "Errors:", :type => :aruba do
   include_context "messages"
-
   context "when any document for input cannot be found" do
-    let(:nonexist_pkg) { "never_never_existing_package_foooooooooo" }
+    let(:nonexist_pkg) { "some_unknown_name" }
 
-    before(:each) { run_texdoc nonexist_pkg }
+    context "with view mode" do
+      before(:each) { run_texdoc nonexist_pkg }
 
-    it 'should result in the "not found" error' do
-      expect(stderr).to include("Unfortunately, there are no good matches for")
+      it 'should result in the "not found" error' do
+        expect(stderr).to include("Unfortunately, there are no good matches for")
+      end
+    end
+
+    context "with mixed mode" do
+      before(:each) { run_texdoc '-m', nonexist_pkg }
+
+      it 'should result in the "not found" error' do
+        expect(stderr).to include("Unfortunately, there are no good matches for")
+      end
     end
   end
 end
